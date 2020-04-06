7ee84d38b826fe75f8e649de5f63ff31ac1af254
Korbinian <kmr.ring@outlook.com>
2020-04-06 13:26:13 +0100

    bugfix

--- src/com/qa/main/DiceRolls.java
+++ src/com/qa/main/DiceRolls.java
@@ -3,15 +3,15 @@ package com.qa.main;
 public class DiceRolls {
 	double roll(int rolls, int diceSide) {
 		double sum = 0;
-		int halfMin = 0;
-		halfMin = diceSide / 2 + 1;
+		int halfMin = diceSide / 2 + 1;
 		for (int i = 0; i < 100000; i++) {
 			for (int j = 0; j < rolls; j++) {
-				int random = (int) (Math.random() * (diceSide));
+				int random = (int) (Math.random() * (diceSide)) + 1;
 				if (random < halfMin) {
 					random = halfMin;
 				}
-				sum += random + 1;
+				System.out.println(random);
+				sum += random;
 			}
 		}
 		sum /= 100000;
--- src/com/qa/main/Runner.java
+++ src/com/qa/main/Runner.java
@@ -3,6 +3,6 @@ package com.qa.main;
 public class Runner {
 	public static void main(String[] Args) {
 		DiceRolls roll = new DiceRolls();
-		System.out.println(roll.roll(1, 8));
+		System.out.println(roll.roll(1, 6));
 	}
 }
